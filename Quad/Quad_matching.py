0b4663541dbc6af5b57b540cae4074db2f72c5ae
Kuan Zhang <118344439+iTheresaApocalypse@users.noreply.github.com>
2024-10-02 17:26:43 +0800

    Update Quad_matching.py

--- Quad/Quad_matching.py
+++ Quad/Quad_matching.py
@@ -7,7 +7,7 @@ def calculate_influence_score(training_info: torch.Tensor, validation_info: torc
     return influence_scores
 
 def matching(result, device):
-    validation_info = torch.load("/mnt/petrelfs/zhangchi/LESS-main/grads/deepseekcoder_>512-slimpajama-30000_old/gsm8k-ckpt30000-sgd-folderlambada-266/dim8192/all_orig.pt")
+    validation_info = torch.load("/mnt/petrelfs/zhangchi/LESS-main/grads/deepseekcoder_>512-slimpajama-30000_old/gsm8k-ckpt30000-sgd-folderlambada-266/dim8192/all_orig.pt") # the path where ihvp is stored
     if not torch.is_tensor(validation_info):
         validation_info = torch.tensor(validation_info)
     validation_info = validation_info.to(device).float()
